9beb6c36276d80aa320c5a4baccb7cdb09ff7359
Robin Appelman <robin@icewind.nl>
2022-08-21 15:23:10 +0200

    flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    naersk,
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages."${system}";
+      naersk-lib = naersk.lib."${system}";
+    in rec {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [rustc cargo bacon pkg-config];
+        buildInputs = with pkgs; [ openssl ];
+      };
+    });
+}
